d976c859056075c4dbea7d10574da05d973ffdac
hjgode <hjgode@gmail.com>
2014-08-15 14:45:08 +0200

    working printing demo label files

---
+++ BTConnection.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Networking;
+using Windows.Networking.Sockets;
+using Windows.Storage.Streams;
+
+using System.Runtime.InteropServices.WindowsRuntime;
+
+using System.IO;
+
+//see http://developer.nokia.com/community/wiki/Windows_Phone_8_communicating_with_Arduino_using_Bluetooth
+
+namespace BTConnection
+{
+    /// <summary>
+    /// Class to control the bluetooth connection to the Arduino.
+    /// </summary>
+    public class ConnectionManager:IDisposable
+    {
+        bool _bIsConnected = false;
+        /// <summary>
+        /// Socket used to communicate with Arduino.
+        /// </summary>
+        private StreamSocket socket=null;
+        
+        /// <summary>
+        /// DataWriter used to send commands easily.
+        /// </summary>
+        private DataWriter dataWriter;
+
+        /// <summary>
+        /// DataReader used to receive messages easily.
+        /// </summary>
+        private DataReader dataReader;
+
+        /// <summary>
+        /// Thread used to keep reading data from socket.
+        /// </summary>
+        private BackgroundWorker dataReadWorker;
+
+        public delegate void ConnectedHandler(HostName deviceHostName);
+        public event ConnectedHandler ConnectDone;
+
+        /// <summary>
+        /// Delegate used by event handler.
+        /// </summary>
+        /// <param name="message">The message received.</param>
+        public delegate void MessageReceivedHandler(string message);
+
+        /// <summary>
+        /// Event fired when a new message is received from Arduino.
+        /// </summary>
+        public event MessageReceivedHandler MessageReceived;
+
+        public ConnectionManager()
+        {
+
+        }
+        /// <summary>
+        /// Initialize the manager, should be called in OnNavigatedTo of main page.
+        /// </summary>
+        void Initialize()
+        {
+            socket = new StreamSocket();
+            dataReadWorker = new BackgroundWorker();
+            dataReadWorker.WorkerSupportsCancellation = true;
+            //dataReadWorker.DoWork += new DoWorkEventHandler(ReceiveMessages);
+        }
+
+        public void Dispose()
+        {
+            this.Disconnect();
+        }
+
+        /// <summary>
+        /// Finalize the connection manager, should be called in OnNavigatedFrom of main page.
+        /// </summary>
+        void Terminate()
+        {
+            addLog("Terminate started");
+            if (dataReadWorker != null)
+            {
+                dataReadWorker.CancelAsync();
+                dataReadWorker.DoWork -= ReceiveMessages;
+            }
+            if (socket != null)
+            {
+                socket.Dispose();
+                socket = null;
+            }
+            dataReadWorker = null;
+            socket = null;
+            MessageReceived(">>>Disconnected");
+            _bIsConnected = false;
+            addLog("Terminate ended");
+        }
+
+        public bool isConnected
+        {
+            get
+            {
+                return _bIsConnected;
+            }
+        }
+        /// <summary>
+        /// Connect to the given host device.
+        /// </summary>
+        /// <param name="deviceHostName">The host device name.</param>
+        public async void Connect(HostName deviceHostName)
+        {
+            if(!_bIsConnected)// if (socket != null)
+            {
+                Initialize();
+                dataReadWorker.DoWork += new DoWorkEventHandler(ReceiveMessages);
+                await socket.ConnectAsync(deviceHostName, "1"); //can we use the SPP UUID too? {00001101-0000-1000-8000-00805F9B34FB} 
+                
+                dataReader = new DataReader(socket.InputStream);
+                dataReader.UnicodeEncoding = Windows.Storage.Streams.UnicodeEncoding.Utf8;
+                dataReadWorker.RunWorkerAsync();
+                
+                dataWriter = new DataWriter(socket.OutputStream);
+                
+                MessageReceived(">>>connected to: " + deviceHostName);
+                ConnectDone(deviceHostName);
+                _bIsConnected = true;
+            }
+        }
+
+        public void Disconnect()
+        {
+            addLog("+++Disconnect");
+            dataReadWorker.DoWork -= ReceiveMessages;
+            dataReadWorker.CancelAsync();
+
+            //dataReader.DetachStream();
+            dataReader.DetachBuffer();
+            dataReader.Dispose();
+            dataReader = null;
+
+            //dataWriter.DetachStream();
+            dataWriter.DetachBuffer();
+            dataWriter.Dispose();
+            dataWriter = null;
+
+            if (socket != null)
+            {
+                socket.Dispose();
+            }
+
+            _bIsConnected = false;
+            socket = null;
+            MessageReceived("Disconnect done");
+            addLog("---Disconnect");
+        }
+        private static byte[] ReadFully(Stream input)
+        {
+            byte[] buffer = new byte[16 * 1024];
+            using (MemoryStream ms = new MemoryStream())
+            {
+                int read;
+                while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    ms.Write(buffer, 0, read);
+                }
+                return ms.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// Receive messages from the Arduino through bluetooth.
+        /// </summary>
+        private async void ReceiveMessages(object sender, DoWorkEventArgs e)
+        {
+            addLog("+++ReceiveMessages started");
+            BackgroundWorker worker = sender as BackgroundWorker;
+            bool bRunMe = true;
+            if (worker.CancellationPending)
+            {
+                e.Cancel = true;
+                addLog("CancellationPending");
+                bRunMe = false;
+            }
+            do
+            {
+                try
+                {
+                    await dataReader.LoadAsync(1);                    
+                    uint dataLen = dataReader.UnconsumedBufferLength;
+                    if (dataLen > 0)
+                    {
+                        //byte[] buffer = ReadFully(socket.InputStream.AsStreamForRead());
+                        //string sMsg = Encoding.UTF8.GetString(buffer, 0, buffer.Length);
+
+                        byte[] buf = new byte[dataLen];
+                        dataReader.ReadBytes(buf);
+                        string sMsg = Encoding.UTF8.GetString(buf, 0, buf.Length);
+                        MessageReceived(sMsg);
+                    }
+                }
+                catch (System.ObjectDisposedException ex)
+                {
+                    addLog("ReceiveMessages ObjectDisposedException: " + ex.Message);
+                    bRunMe = false;
+                }
+                catch (TaskCanceledException ex)
+                {
+                    addLog("ReceiveMessages TaskCanceledException: " + ex.Message);
+                    bRunMe = false;
+                }
+                catch (System.NullReferenceException ex)
+                {
+                    addLog("ReceiveMessages NullReferenceException: " + ex.Message);
+                    bRunMe = false;
+                }
+                catch (Exception ex)
+                {
+                    addLog("ReceiveMessages Exception: " + ex.Message);
+                    bRunMe = false;
+                }
+
+                /*
+                // Read first byte (length of the subsequent message, 255 or less). 
+                uint sizeFieldCount = await dataReader.LoadAsync(1);
+                if (sizeFieldCount != 1)
+                {
+                    // The underlying socket was closed before we were able to read the whole data. 
+                    return;
+                }
+
+                // Read the message. 
+                uint messageLength = dataReader.ReadByte();
+                uint actualMessageLength = await dataReader.LoadAsync(messageLength);
+                if (messageLength != actualMessageLength)
+                {
+                    // The underlying socket was closed before we were able to read the whole data. 
+                    return;
+                }
+                // Read the message and process it.
+                string message = dataReader.ReadString(actualMessageLength);
+                MessageReceived(message);
+                */
+            } while (bRunMe);
+            addLog("---ReceiveMessages ended");
+        }
+
+        /// <summary>
+        /// Send command to the Arduino through bluetooth.
+        /// </summary>
+        /// <param name="command">The sent command.</param>
+        /// <returns>The number of bytes sent</returns>
+        public async Task<uint> send(string command)
+        {
+            uint sentCommandSize = 0;
+            if (dataWriter != null)
+            {
+                //uint commandSize = dataWriter.MeasureString(command);
+                //dataWriter.WriteByte((byte)commandSize);
+                sentCommandSize = dataWriter.WriteString(command);
+                await dataWriter.StoreAsync();
+                await dataWriter.FlushAsync();
+            }
+            return sentCommandSize;
+        }
+        public async Task<uint> send(IBuffer buffer)
+        {
+            uint sentCommandSize = 0;
+            if (dataWriter != null)
+            {
+                //uint commandSize = dataWriter.MeasureString(command);
+                //dataWriter.WriteByte((byte)commandSize);
+                dataWriter.WriteBuffer(buffer, 0, buffer.Length);
+                await dataWriter.StoreAsync();
+                await dataWriter.FlushAsync();
+                sentCommandSize = (uint) buffer.Length;
+            }
+            return sentCommandSize;
+        }
+        public async Task<uint> send(byte[] buffer)
+        {
+            uint sentCommandSize = 0;
+            if (dataWriter != null)
+            {
+                //uint commandSize = dataWriter.MeasureString(command);
+                //dataWriter.WriteByte((byte)commandSize);
+                dataWriter.WriteBuffer(buffer.AsBuffer());
+                await dataWriter.StoreAsync();
+                await dataWriter.FlushAsync();
+                sentCommandSize = (uint) buffer.Length;
+            }
+            return sentCommandSize;
+        }
+
+
+        static void addLog(string s){
+            System.Diagnostics.Debug.WriteLine(s);
+        }
+    }
+}
--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -14,58 +14,179 @@ using Windows.Foundation;
 using Windows.Networking.Sockets;
 using System.Runtime.InteropServices.WindowsRuntime;
 
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using System.IO;
+
+using BTConnection;
+
 namespace btPrint4wp
 {
     public partial class MainPage : PhoneApplicationPage
     {
+        BTConnection.ConnectionManager btConn;
+        PeerInformation peerInformation;
+        public string sFileName = "";
+
         // Constructor
         public MainPage()
         {
             InitializeComponent();
+            btConn = new ConnectionManager();
+            btConn.ConnectDone += btConn_ConnectDone;
+            btConn.MessageReceived += btConn_MessageReceived;
 
-            //var allowBluetooth = Windows.Networking.Proximity.PeerFinder.allowBluetooth;
-            //Windows.Networking.Proximity.PeerFinder.allowBluetooth = allowBluetooth;
-
+            /*
+            try
+            {
+                var allowBluetooth = Windows.Networking.Proximity.PeerFinder.allowBluetooth;
+                Windows.Networking.Proximity.PeerFinder.allowBluetooth = allowBluetooth;
+            }
+            catch (Exception ex) { 
+                addLog("Exception: " + ex.Message);
+            }
+            */
             // Sample code to localize the ApplicationBar
             //BuildLocalizedApplicationBar();
         }
 
-        private async void btSearch_Click(object sender, RoutedEventArgs e)
+        void btConn_ConnectDone(Windows.Networking.HostName deviceHostName)
         {
-            PeerFinder.AlternateIdentities["Bluetooth:Paired"] = "";
-            var peerList = await PeerFinder.FindAllPeersAsync();
-            if (peerList.Count > 0)
-            {
-                textBoxPeer.Text = peerList[0].DisplayName;
-                for (int i = 0; i < peerList.Count; i++)
-                {
-                    System.Diagnostics.Debug.WriteLine(string.Format("{0}: '{1}'", i, peerList[i].DisplayName));
-                }
-            }
-            else MessageBox.Show("No active peers");
+            //await btConn.send("Hello \n");
+            addLog("Connected to " + deviceHostName);
+            //await btConn.SendCommand("Hello\n");
+            //System.Threading.Thread.Sleep(2000);
         }
 
-        private async void btConnect_Click(object sender, RoutedEventArgs e)
+        private async void btSearch_Click(object sender, RoutedEventArgs e)
         {
-            var peerList = await PeerFinder.FindAllPeersAsync();
-            PeerInformation pi = peerList[0];
-            if (peerList.Count > 0)
-                textBoxPeer.Text = peerList[0].DisplayName;
-            StreamSocket socket = new StreamSocket();
-            //await socket.ConnectAsync(peerList[0].HostName, "0");
             try
             {
-                await socket.ConnectAsync(pi.HostName, "1");//"{21EC2020-3AEA-1069-A2DD-08002B30309D}"); //SPP connect
-                string s = "Hello\n";
-                
-                var buffer = System.Text.Encoding.UTF8.GetBytes(s);
-                uint u = await socket.OutputStream.WriteAsync(WindowsRuntimeBufferExtensions.AsBuffer(buffer));
-                System.Diagnostics.Debug.WriteLine("written: " + u.ToString());
+                //PeerFinder.AlternateIdentities["Bluetooth:Paired"] = "";
+                PeerFinder.AlternateIdentities["Bluetooth:SDP"] = "{00001101-0000-1000-8000-00805F9B34FB}";
+
+                var peerList = await PeerFinder.FindAllPeersAsync();
+                if (peerList.Count > 0)
+                {
+                    textBoxPeer.Text = peerList[0].DisplayName;
+                    for (int i = 0; i < peerList.Count; i++)
+                    {
+                        System.Diagnostics.Debug.WriteLine(string.Format("{0}: '{1}'", i, peerList[i].DisplayName));
+                    }
+                    ///ToDo: add code
+                    peerInformation = peerList[0];
+                }
+                else
+                {
+                    MessageBox.Show("No active peers");
+                }
             }
             catch (Exception ex)
             {
-                System.Diagnostics.Debug.WriteLine("Exception: " + ex.Message);
+                if ((uint)ex.HResult == 0x8007048F)
+                {
+                    MessageBox.Show("Bluetooth is turned off");
+                }
             }
+        }
+
+        private void btConnect_Click(object sender, RoutedEventArgs e)
+        {
+            if (peerInformation == null)
+            {
+                addLog("Please use search first");
+                ShellToast toast = new ShellToast();
+                toast.Content = "Please use search first";
+                toast.Title = "Error";
+                toast.Show();
+                return;
+            }
+            if (btConn.isConnected)
+            {
+                addLog("Already connected. Disconnect first!");
+                return;
+            }
+            btConn.Connect(peerInformation.HostName);
+            return;
+        }
+
+        private async void btnPrint_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (btConn.isConnected) { 
+                    //await btConn.send("Hello 2\n");
+                    if (sFileName != "")
+                    {
+                        printFile(sFileName);
+                        addLog("Send done");
+                    }
+                    else
+                    {
+                        addLog("no print file selected");
+                    }
+                }
+                else
+                {
+                    addLog("NOT CONNECTED");
+                }
+            }
+            catch (Exception ex) { addLog("Exception: " + ex.Message); }
+        }
+
+        async void printFile(string _filename)
+        {
+            StorageFolder PrintfilesFolder = await Windows.ApplicationModel.Package.Current.InstalledLocation.GetFolderAsync("printfiles");
+            StorageFile printfile = await PrintfilesFolder.GetFileAsync(_filename);
+            if (printfile != null)
+            {
+                byte[] buf = await ReadFileContentsAsync(printfile);
+                await btConn.send(buf);
+                addLog("printFile done");
+            }
+        }
+
+        public async Task<byte[]> ReadFileContentsAsync(StorageFile _fileName)
+        {
+            try
+            {
+
+                var file = await _fileName.OpenReadAsync();
+                Stream stream = file.AsStreamForRead();
+                byte[] buf = new byte[stream.Length];
+                int iRead = stream.Read(buf, 0, buf.Length);
+                addLog("read file = " + iRead.ToString());
+                return buf;
+            }
+            catch (Exception)
+            {
+                return new byte[0];
+            }
+        }
+
+        void addLog(string s)
+        {
+            string t = txtLog.Text;
+            txtLog.Text = s + "\r\n" + t;
+            System.Diagnostics.Debug.WriteLine(s);
+        }
+
+        void btConn_MessageReceived(string message)
+        {
+            addLog(message);
+        }
+
+        private void btDisconnect_Click(object sender, RoutedEventArgs e)
+        {
+            btConn.Disconnect();
+            //btConn = null;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Page1.xaml", UriKind.Relative));
+            
         }
 
         // Sample code for building a localized ApplicationBar
